1957ead03fe2a40d23cc0c486c271a48c596d7e5
DheerajKholia <dheraj.kholia@yahoo.com>
2020-01-12 17:51:48 +0530

    Using JPA entities

--- src/main/java/com/dheeraj/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/dheeraj/webservices/restfulwebservices/user/User.java
@@ -3,11 +3,19 @@ package com.dheeraj.webservices.restfulwebservices.user;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+
 @ApiModel(description = "All details of user")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
